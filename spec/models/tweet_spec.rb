129f1b5ece9d8e911304370bb6ab1a32e50b4ced
Alisher Sadikov <alishersadikov@gmailcom>
2016-12-05 22:13:33 -0700

    build tweet model with user association

---
+++ spec/models/tweet_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Tweet, type: :model do
+  describe "validations" do
+    it "is invalid without user_id" do
+      tweet = Tweet.new(content: "content")
+
+      expect(tweet).to be_invalid
+    end
+
+    it "is invalid with content and user_id" do
+      user = User.create(name: "ali", email: "al@al.com", password: "password1", password_confirmation: "password1")
+      tweet = Tweet.new(content: "content", user_id: user.id)
+
+      expect(tweet).to be_valid
+    end
+
+    it "is invalid with content too long" do
+      user = User.create(name: "ali", email: "al@al.com", password: "password1", password_confirmation: "password1")
+      content = "a" * 141
+      tweet = Tweet.new(content: content, user_id: user.id)
+
+      expect(tweet).to be_invalid
+    end
+  end
+end
